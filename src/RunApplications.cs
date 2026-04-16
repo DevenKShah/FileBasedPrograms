5de7dadfaae45617d9b57bb6db709ce96a24ab4d
Deven Shah <devenshah1974@gmail.com>
2026-04-16 11:11:36 +0100

    Updated RunApplications to optional restore and release

--- src/RunApplications.cs
+++ src/RunApplications.cs
@@ -2,15 +2,23 @@
 #:property ToolCommandName=run-apps
 #:property NoDefaultExcludes=true
 #:property PackageOutputPath=./nupkg
+#:property Version=1.0.1
 
-// dotnet pack RunApplications.cs  --output ./nupkg --force
-// dotnet tool install -g --add-source ./nupkg RunApplications
+
 
 using System;
 using System.Diagnostics;
 
+Console.WriteLine("Running applications 1.0.1");
+
 var rootDir = @"C:\Git\Esfa\";
 const string terminalName = nameof(terminalName);
+
+var noRestore = "--no-restore";
+if (args.Contains("restore", StringComparer.OrdinalIgnoreCase)) noRestore = string.Empty;
+
+var release = "--release";
+if (args.Contains("debug", StringComparer.OrdinalIgnoreCase)) release = string.Empty;
 
 var changedRootDir = args.Contains("rootdir", StringComparer.OrdinalIgnoreCase);
 if (changedRootDir)
@@ -20,31 +28,30 @@ if (changedRootDir)
     rootDir = string.IsNullOrWhiteSpace(dir) ? rootDir : dir.Trim();
 }
 
-
-Process.Start("wt", $"-w {terminalName} nt --title \"5111 RoatpV2 API Inner\" -d \"{rootDir}das-roatp-api\\src\\SFA.DAS.RoATP.Api\" -c dotnet run");
-Process.Start("wt", $"-w {terminalName} nt --title \"37952 RoATP Service\" -d \"{rootDir}das-roatp-service\\src\\SFA.DAS.RoATPService.Application.Api\" -c dotnet run --release");
-Process.Start("wt", $"-w {terminalName} nt --title \"5008 LocApi\" -d \"{rootDir}das-location-api\\src\\SFA.DAS.Location.Api\" -c dotnet run --release");
-Process.Start("wt", $"-w {terminalName} nt --title \"5011 CrsInApi\" -d \"{rootDir}das-courses-api\\src\\SFA.DAS.Courses.Api\" -c dotnet run --release");
+Process.Start("wt", $"-w {terminalName} nt --title \"5111 RoatpV2 API Inner\" -d \"{rootDir}das-roatp-api\\src\\SFA.DAS.RoATP.Api\" -c dotnet run --release {noRestore}");
+Process.Start("wt", $"-w {terminalName} nt --title \"37952 RoATP Service\" -d \"{rootDir}das-roatp-service\\src\\SFA.DAS.RoATPService.Application.Api\" -c dotnet run --release {noRestore}");
+Process.Start("wt", $"-w {terminalName} nt --title \"5008 LocApi\" -d \"{rootDir}das-location-api\\src\\SFA.DAS.Location.Api\" -c dotnet run --release {noRestore}");
+Process.Start("wt", $"-w {terminalName} nt --title \"5011 CrsInApi\" -d \"{rootDir}das-courses-api\\src\\SFA.DAS.Courses.Api\" -c dotnet run --release {noRestore}");
 
 // Console.WriteLine("Do you want to run the Course Management? (Y/N)");
 // var runCourseManagement = Console.ReadKey().Key == ConsoleKey.Y;
 var runCourseManagement = args.Contains("cm", StringComparer.OrdinalIgnoreCase);
 if (runCourseManagement)
 {
-    Process.Start("wt", $"wt -w {terminalName} nt --title \"5334 CM Outer\" -d \"{rootDir}das-apim-endpoints\\src\\RoatpCourseManagement\\SFA.DAS.RoatpCourseManagement.Api\" -c dotnet run --release");
-    Process.Start("wt", $"wt -w {terminalName} nt --title \"5021 CM Web\" -d \"{rootDir}das-roatp-coursemanagement-web\\src\\SFA.DAS.Roatp.CourseManagement.Web\" -c dotnet run --release");
+    Process.Start("wt", $"wt -w {terminalName} nt --title \"5334 CM Outer\" -d \"{rootDir}das-apim-endpoints\\src\\RoatpCourseManagement\\SFA.DAS.RoatpCourseManagement.Api\" -c dotnet run {release} {noRestore}");
+    Process.Start("wt", $"wt -w {terminalName} nt --title \"5021 CM Web\" -d \"{rootDir}das-roatp-coursemanagement-web\\src\\SFA.DAS.Roatp.CourseManagement.Web\" -c dotnet run {release} {noRestore}");
 }
 
 var runFAT = args.Contains("fat", StringComparer.OrdinalIgnoreCase);
 if (runFAT)
 {
-    Process.Start("wt", $"wt -w {terminalName} nt --title \"5003 FAT Outer\" -d \"{rootDir}das-apim-endpoints\\src\\FindApprenticeshipTraining\\SFA.DAS.FindApprenticeshipTraining.Api\" -c dotnet run --release");
-    Process.Start("wt", $"wt -w {terminalName} nt --title \"5004 FAT Web\" -d \"{rootDir}das-findapprenticeshiptraining\\src\\SFA.DAS.FAT.Web\" -c dotnet run --release");
+    Process.Start("wt", $"wt -w {terminalName} nt --title \"5003 FAT Outer\" -d \"{rootDir}das-apim-endpoints\\src\\FindApprenticeshipTraining\\SFA.DAS.FindApprenticeshipTraining.Api\" -c dotnet run {release} {noRestore}");
+    Process.Start("wt", $"wt -w {terminalName} nt --title \"5004 FAT Web\" -d \"{rootDir}das-findapprenticeshiptraining\\src\\SFA.DAS.FAT.Web\" -c dotnet run {release} {noRestore}");
 }
 
 var runAdmin = args.Contains("admin", StringComparer.OrdinalIgnoreCase);
 if (runAdmin)
 {
-    Process.Start("wt", $"wt -w {terminalName} nt --title \"5005 Admin Outer\" -d \"{rootDir}das-apim-endpoints\\src\\Admin\\SFA.DAS.Admin.Api\" -c dotnet run --release");
-    Process.Start("wt", $"wt -w {terminalName} nt --title \"5006 Admin Web\" -d \"{rootDir}das-admin\\src\\SFA.DAS.Admin.Web\" -c dotnet run --release");
+    Process.Start("wt", $"wt -w {terminalName} nt --title \"5005 Admin Outer\" -d \"{rootDir}das-apim-endpoints\\src\\Admin\\SFA.DAS.Admin.Api\" -c dotnet run {release} {noRestore}");
+    Process.Start("wt", $"wt -w {terminalName} nt --title \"5006 Admin Web\" -d \"{rootDir}das-admin\\src\\SFA.DAS.Admin.Web\" -c dotnet run {release} {noRestore}");
 }
